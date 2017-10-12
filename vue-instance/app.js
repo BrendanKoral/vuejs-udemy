7937f81fef2e665850bfa58f1f1b66c5d8b9d3fc
Brendan Koral <brendanhkoral@gmail.com>
2017-10-11 22:09:47 -0500

    Modify multiple Vue instances

--- vue-instance/app.js
+++ vue-instance/app.js
@@ -1,5 +1,5 @@
-new Vue({
-  el: '#app',
+let vm1 = new Vue({
+  el: '#app1',
   data: {
     title: 'The VueJS Instance',
     showParagraph: false
@@ -24,3 +24,15 @@ new Vue({
     }
   }
 });
+
+let vm2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'The second instance'
+  },
+  methods: {
+    onChange: function() {
+      vm1.title = 'Hello world'
+    }
+  }
+})
